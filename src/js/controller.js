d634930aff0b02a6e718ae87894715e4ddd3e60c
Slson <silson0072@gmail.com>
2023-01-05 20:22:07 +0545

    v0.0.1 (UI Only)

---
+++ src/js/controller.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('.container');
+const header = document.querySelector('.header');
+const headerLogo = document.querySelector('.header__logo');
+const main = document.querySelector('.main');
+const leftContainer = document.querySelector('.main__left');
+const rightContainer = document.querySelector('.main__right');
+
+headerLogo.addEventListener('click', () => {
+  leftContainer.classList.toggle('hidden');
+  rightContainer.classList.toggle('margin-left');
+});
+
+// When the width of the screen is less than 768px, the left container is hidden
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 960) {
+    leftContainer.classList.add('hidden');
+    rightContainer.classList.add('margin-left');
+
+  }
+});
