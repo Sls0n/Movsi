a202d40e435f7ea0173baa78cf90c3cd445c529a
Slson <silson0072@gmail.com>
2023-01-07 20:48:25 +0545

    ui-only

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,7 @@
 import icons from 'url:../img/sprite.svg';
 import image from 'url:../img/sample.jpg';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 const container = document.querySelector('.container');
 const header = document.querySelector('.header');
@@ -8,51 +10,30 @@ const main = document.querySelector('.main');
 const leftContainer = document.querySelector('.main__left');
 const rightContainer = document.querySelector('.main__right');
 const movies = document.querySelectorAll('.movie');
+const mainMenu = document.querySelector('.mainMenu');
+const crossMenu = document.querySelector('.crossMenu');
 
 headerLogo.addEventListener('click', () => {
   leftContainer.classList.toggle('hidden');
   rightContainer.classList.toggle('margin-left');
+
+  if (crossMenu.classList.contains('hidden')) {
+    crossMenu.classList.remove('hidden');
+    mainMenu.classList.add('hidden');
+  } else {
+    crossMenu.classList.add('hidden');
+    mainMenu.classList.remove('hidden');
+  }
 });
 
 // When the width of the screen is less than 768px, the left container is hidden
 
-window.addEventListener('resize', () => {
-  if (window.innerWidth <= 960) {
-    leftContainer.classList.add('hidden');
-    rightContainer.classList.add('margin-left');
-  }
-});
-
 if (window.innerWidth <= 960) {
   leftContainer.classList.add('hidden');
   rightContainer.classList.add('margin-left');
-}
+  rightContainer.style.filter = 'blur(0px)';
 
-movies.forEach(function (movie) {
-  movie.addEventListener('mouseover', function () {
-    const markup = `
-      <img class="movie__image" src="${image}" alt="movie-1" />
-      
-      <svg class="icon--heart movie__heart">
-        <use xlink:href="${icons}#icon-heart"></use>
-      </svg>
-      
-      <svg class="icon--maximize movie__maximize">
-        <use xlink:href="${icons}#icon-maximize"></use>
-      </svg>
-      
-      <div class="movie__info">
-        <div class="movie__info--title">Prey The way of water</div>
-      </div>
-      
-      <div class="movie__info--rating">
-        <svg class="icon icon--title">
-          <use xlink:href="${icons}#icon-star"></use>
-        </svg>
-        7.6
-      </div>
-`;
-    movie.innerHTML = markup;
+  headerLogo.addEventListener('click', () => {
+    rightContainer.style.filter = 'blur(5px)';
   });
-  // When the mouse leaves the movie, the markup is removed
-});
+}
