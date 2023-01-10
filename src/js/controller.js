fd5fe4035e644a7239c5ffbe78c467d8a7994d3c
Slson <silson0072@gmail.com>
2023-01-11 00:04:32 +0545

    v0.1.1

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,17 @@ import navigationView from './views/navigationView.js';
 import sidebarView from './views/sidebarView.js';
 import containerView from './views/containerView.js';
 import searchView from './views/searchView.js';
+import { API_KEY } from './API_KEY.js';
+
+const getMovie = async function () {
+  const response = await fetch(`https://api.themoviedb.org/3/movie/76341?api_key=${API_KEY}`);
+  if (!response.ok) throw new Error(`Problem getting movie data (${response.status})`);
+
+  const data = await response.json();
+  console.log(data);
+};
+
+getMovie();
 
 const init = function () {
   floatingView.init();
